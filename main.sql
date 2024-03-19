d6f7a3b593b9a495e8fef6f608753841b7442f82
JoseCL <josecl@galileo.edu>
2024-03-18 18:50:56 -0600

    Create table de tabla Date Dimension

---
+++ main.sql
@@ -0,0 +1,12 @@
+
+
+
+-- TABLA DATE DIMENSION
+CREATE TABLE DATE_DIMENSION(
+    DATE_KEY INT AUTO_INCREMENT PRIMARY KEY,
+	DATE DATE,
+    DAY_OF_WEEK ENUM('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'),
+    CALENDAR_MONTH ENUM('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
+    CALENDAR_QUARTER ENUM('Q1', 'Q2', 'Q3', 'Q4'),
+    CALENDAR_YEAR INT
+);
